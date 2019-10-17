bb0413fbf8e3dc5ba7548be23b27b73aa6a5135f
Jonas Mock <info@jonasmock.de>
2019-10-17 22:35:32 +0200

    cmd arguments / implemented manual mode selection

--- scripts/Main.py
+++ scripts/Main.py
@@ -1,33 +1,130 @@
-from classify import Classify 
-from trainModel import TrainModel
-from predictService import PredictService 
-import configparser
-from globalServices import GlobalServices
+class Main:
+        
+        import configparser
+        from globalServices import GlobalServices
+        import os
 
-args = {"pathToConfig":"/Users/jonas/Desktop/config.ini",
-        }
+        __slots__ = ['pathToConfig', 'mode']
+    
+    
+        def __init__(self, **kwargs):
 
-init = GlobalServices(**args)
+                print("\nThis is the constructor method of \""+type(self).__name__+"\" class.\n")
+                print("The following args are optional: ", self.__slots__,"")
 
-cfg = init.configToDict()
+                for key, value in kwargs.items():
 
-print(int(init.readConfig("Global","lastPrediction")))
+                        try:
+                                setattr(self, key, value)
+                                pass
 
-GlobalServices(**args).writeConfig("Global", "lastPrediction", 12345)
+                        except Exception as e:
+                                print("Can't init object.\n")
+                                raise Exception(e)
 
-#init.writeConfig("scripts/config.ini", "DEFAULT", "Property", "Newvalues")
+                try:
+
+                        self.selectMode(self.initConfig(getattr(self, "pathToConfig")), True)
+                        getattr(self, "mode")
+
+                        pass
+
+                except Exception:
+
+                        print("\n\n#####Welcome#####\n")
+
+                        pathToConfig = input("Please enter path to config.ini file:")
+
+                        while self.os.path.isfile(pathToConfig) != True:
+
+                                pathToConfig = input("ERROR path is not vaild.\nPlease enter path to config.ini file:")
+                                if pathToConfig[-3:] != "ini":
+                                        pathToConfig = ""
+
+                        print("\nInitialize config from \"", pathToConfig ,"\"\n")
+  
+                        self.selectMode(self.initConfig(pathToConfig), False)
+
+                pass
+
+
+        # Starts selected mode
+        def selectMode(self, configDict, automatic):
+
+                mode = ""
+
+                if automatic == True:
+
+                        mode = getattr(self, "mode")
+
+                else:
+
+                        while not mode:
+
+                                mode = input("Enter mode. \'classify\' , \'train\' or \'predict\'")
+                                if mode == "classify" or mode == "train" or mode == "predict":
+                                        print("Correct mode chosen.")
+                                else:
+                                        mode = ""
+                                        
+                                        
+
+                if mode == "classify": 
+
+                        from classify import Classify 
+
+                        classify = Classify(**dict(configDict["Classify"]))
+                        classify.classifyMultipleImages()
+
+                elif mode == "train":
+
+                        from trainModel import TrainModel
+                        train = TrainModel(**dict(configDict["Train"]))
+                        train.createModel()
+
+                elif mode == "predict":
+
+                        from predictService import PredictService
+                        predict = PredictService(**dict(configDict["Predict"]))
+                        predict.downloadImage()
+                        predict.predict()
+
+
+                pass
+
+        def initConfig(self, pathToConfig):
+
+                args = {"pathToConfig":pathToConfig,}
+
+                init = self.GlobalServices(**args)
+
+                return init.configToDict()
+
+
+        pass
+
+#########################################################
+
+import sys
+
+args = {}
+print(sys.argv.index)
+if len(sys.argv) == 3:
+        args = {"pathToConfig":sys.argv[1], "mode":sys.argv[2],}
+
+print(args)
+run = Main(**args)
 
 
 
-#classify = Classify(**dict(cfg["Classify"]))
-#classify.classifyMultipleImages()
 
-#train = TrainModel(**dict(cfg["Train"]))
-#train.createModel()
 
-#predict = PredictService(**dict(cfg["Predict"]))
-#predict.downloadImage()
-#predict.predict()
+                
+
+
+
+
+
 
 
 
